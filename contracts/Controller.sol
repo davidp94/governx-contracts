28680bb37abebf4a8dc1a7017228ca3de25aad31
Nick Dodson <thenickdodson@gmail.com>
2017-08-08 15:20:01 -0400

    Update Controller.sol

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -1,19 +1,7 @@
 pragma solidity 0.4.15;
 
-contract Proxy {
-    event Forwarded (address indexed destination, uint value, bytes data);
-    event Received (address indexed sender, uint value);
-    function forward(address destination, uint value, bytes data);
-}
-
-contract Rules {
-    function canPropose(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool);
-    function canVote(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool);
-    function canExecute(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool);
-    function votingWeightOf(address _sender, uint256 _value, uint256 _proposalID, uint256 _index, uint256 _data) public constant returns (uint256);
-    function voteOffset(address _sender, uint256 _value, uint256 _proposalID) public constant returns (uint256);
-    function executionOffset(address _sender, uint256 _value, uint256 _proposalID) public constant returns (uint256);
-}
+import "IRules.sol";
+import "IProxy.sol";
 
 contract Proposable {
     modifier hasMoment (uint256 _proposalID) { recordMoment(_proposalID); _; }
