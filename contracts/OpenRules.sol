a41508852b20d9ffe73c03c6e2fce4ab0b8d3cbc
Nick Dodson <thenickdodson@gmail.com>
2017-08-08 14:13:45 -0400

    Create OpenRules.sol

---
+++ contracts/OpenRules.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.4.15;
+
+import "Rules.sol";
+
+contract OpenRules is Rules {
+    function canPropose(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool) { return true; }
+    function canVote(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool)  { return true; }
+    function canExecute(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool)  { return true; }
+    function votingWeightOf(address _sender, uint256 _value, uint256 _proposalID, uint256 _index, uint256 _data) public constant returns (uint256)  { return 1; }
+    function voteOffset(address _sender, uint256 _value, uint256 _proposalID) public constant returns (uint256)  { return 0; }
+    function executionOffset(address _sender, uint256 _value, uint256 _proposalID) public constant returns (uint256) { return 0; }
+}
