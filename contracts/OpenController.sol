eefd497aa29da60bd822aab4ef28e5f9b29fb3b8
Nick Dodson <thenickdodson@gmail.com>
2017-08-09 17:54:36 -0400

    Update and rename OpenRules.sol to OpenController.sol

--- contracts/OpenController.sol
+++ contracts/OpenController.sol
@@ -1,8 +1,8 @@
 pragma solidity 0.4.15;
 
-import "IRules.sol";
+import "Controller.sol";
 
-contract OpenRules is Rules {
+contract OpenController is Controller {
     function canPropose(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool) {
         return true;
     }
@@ -17,13 +17,5 @@ contract OpenRules is Rules {
  
     function votingWeightOf(address _sender, uint256 _value, uint256 _proposalID, uint256 _index, uint256 _data) public constant returns (uint256)  {
         return 1;
-    }
-   
-    function voteOffset(address _sender, uint256 _value, uint256 _proposalID) public constant returns (uint256)  {
-        return 0;
-    }
-   
-    function executionOffset(address _sender, uint256 _value, uint256 _proposalID) public constant returns (uint256) {
-        return 0;
     }
 }
