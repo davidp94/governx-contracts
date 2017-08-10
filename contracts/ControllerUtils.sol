85611d12675cd437369ec28e393055ec13278180
Nick Dodson <thenickdodson@gmail.com>
2017-08-10 09:20:10 -0400

    Update ControllerUtils.sol

--- contracts/ControllerUtils.sol
+++ contracts/ControllerUtils.sol
@@ -1,35 +1,39 @@
 pragma solidity 0.4.15;
 
-contract ControllerUtils is Controller {
+import "IController.sol";
+
+contract ControllerUtils {
     function proxyOf(uint256 _proposalID, uint256 _c) constant returns (address) {
-        return address(dataOf(_proposalID, _c));
+        return address(IController(this).dataOf(_proposalID, _c));
     }
 
     function destinationOf(uint256 _proposalID, uint256 _c) constant returns (address) {
-        return address(dataOf(_proposalID, _c + 1));
+        return address(IController(this).dataOf(_proposalID, _c + 1));
     }
     
     function valueOf(uint256 _proposalID, uint256 _c) constant returns (uint256) {
-        return uint256(dataOf(_proposalID, _c + 2));
+        return uint256(IController(this).dataOf(_proposalID, _c + 2));
     }
     
     function dataLengthOf(uint256 _proposalID, uint256 _c) constant returns (uint256) {
-        return uint256(dataOf(_proposalID, _c + 3));
+        return uint256(IController(this).dataOf(_proposalID, _c + 3));
     }
     
     function signatureOf(uint256 _proposalID, uint256 _c) constant returns (bytes4) {
-        return bytes4(dataOf(_proposalID, _c + 4));
+        return bytes4(IController(this).dataOf(_proposalID, _c + 4));
     }
     
     function latestSenderOf(uint256 _proposalID) constant returns (address) {
-        return momentSenderOf(_proposalID, numMomentsOf(_proposalID) - 1);
+        return IController(this).momentSenderOf(_proposalID, IController(this).numMomentsOf(_proposalID) - 1);
     }
     
     function executionTimeOf(uint256 _proposalID) constant returns (uint256) {
-        if (hasExecuted(_proposalID)) return momentTimeOf(_proposalID, numMomentsOf(_proposalID) - 1);
+        if (IController(this).hasExecuted(_proposalID))
+            return IController(this).momentTimeOf(_proposalID, IController(this).numMomentsOf(_proposalID) - 1);
     }
 
     function executionBlockOf(uint256 _proposalID) constant returns (uint256) {
-        if (hasExecuted(_proposalID)) return momentBlockOf(_proposalID, numMomentsOf(_proposalID) - 1);
+        if (IController(this).hasExecuted(_proposalID))
+            return IController(this).momentBlockOf(_proposalID, IController(this).numMomentsOf(_proposalID) - 1);
     }
 }
