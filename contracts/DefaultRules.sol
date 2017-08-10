6cabf80d71a4ce2022c0f1ff0b153e4a1f8e00ae
Nick Dodson <thenickdodson@gmail.com>
2017-08-10 08:49:24 -0400

    Create DefaultRules.sol

---
+++ contracts/DefaultRules.sol
@@ -0,0 +1,30 @@
+pragma solidity 0.4.15;
+
+import "IRules.sol";
+
+
+contract DefaultRules is IRules {
+    function canPropose(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool) {
+        return false;
+    }
+
+    function canVote(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool)  {
+        return false;
+    }
+
+    function canExecute(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool)  {
+        return false;
+    }
+ 
+    function votingWeightOf(address _sender, uint256 _value, uint256 _proposalID, uint256 _index, uint256 _data) public constant returns (uint256)  {
+        return 1;
+    }
+   
+    function voteOffset(address _sender, uint256 _value, uint256 _proposalID) public constant returns (uint256)  {
+        return 0;
+    }
+   
+    function executionOffset(address _sender, uint256 _value, uint256 _proposalID) public constant returns (uint256) {
+        return 0;
+    }
+}
