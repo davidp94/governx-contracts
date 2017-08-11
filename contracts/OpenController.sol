0d7f7fa81b8c38e84a73a41563ec1337c92616f4
Nick Dodson <thenickdodson@gmail.com>
2017-08-11 10:14:41 -0400

    Update OpenController.sol

--- contracts/OpenController.sol
+++ contracts/OpenController.sol
@@ -3,19 +3,19 @@ pragma solidity 0.4.15;
 import "Controller.sol";
 
 contract OpenController is Controller {
-    function canPropose(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool) {
+    function canPropose(address _sender, uint256 _proposalID) public constant returns (bool) {
         return true;
     }
 
-    function canVote(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool)  {
+    function canVote(address _sender, uint256 _proposalID) public constant returns (bool)  {
         return true;
     }
 
-    function canExecute(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool)  {
+    function canExecute(address _sender, uint256 _proposalID) public constant returns (bool)  {
         return true;
     }
  
-    function votingWeightOf(address _sender, uint256 _value, uint256 _proposalID, uint256 _index, uint256 _data) public constant returns (uint256)  {
+    function votingWeightOf(address _sender, uint256 _proposalID, uint256 _index, uint256 _data) public constant returns (uint256)  {
         return 1;
     }
 }
