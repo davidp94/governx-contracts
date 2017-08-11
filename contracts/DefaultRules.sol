49646d35a6681d37d21acda854ea41b88fe2f8eb
Nick Dodson <thenickdodson@gmail.com>
2017-08-11 10:17:04 -0400

    Update DefaultRules.sol

--- contracts/DefaultRules.sol
+++ contracts/DefaultRules.sol
@@ -4,27 +4,27 @@ import "IRules.sol";
 
 
 contract DefaultRules is IRules {
-    function canPropose(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool) {
+    function canPropose(address _sender, uint256 _proposalID) public constant returns (bool) {
         return false;
     }
 
-    function canVote(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool)  {
+    function canVote(address _sender, uint256 _proposalID) public constant returns (bool)  {
         return false;
     }
 
-    function canExecute(address _sender, uint256 _value, uint256 _proposalID) public constant returns (bool)  {
+    function canExecute(address _sender, uint256 _proposalID) public constant returns (bool)  {
         return false;
     }
  
-    function votingWeightOf(address _sender, uint256 _value, uint256 _proposalID, uint256 _index, uint256 _data) public constant returns (uint256)  {
+    function votingWeightOf(address _sender, uint256 _proposalID, uint256 _index, uint256 _data) public constant returns (uint256)  {
         return 1;
     }
    
-    function voteOffset(address _sender, uint256 _value, uint256 _proposalID) public constant returns (uint256)  {
+    function voteOffset(address _sender, uint256 _proposalID) public constant returns (uint256)  {
         return 0;
     }
    
-    function executionOffset(address _sender, uint256 _value, uint256 _proposalID) public constant returns (uint256) {
+    function executionOffset(address _sender, uint256 _proposalID) public constant returns (uint256) {
         return 0;
     }
 }
