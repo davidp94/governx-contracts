7c362cb6a4e14858e3e08d8edf77313513b69128
Nick Dodson <thenickdodson@gmail.com>
2017-08-14 16:19:35 -0400

    Update IController.sol

--- contracts/IController.sol
+++ contracts/IController.sol
@@ -5,7 +5,7 @@ contract IController {
     function numMomentsOf(uint256 _proposalID) public constant returns (uint256);
     function momentSenderOf(uint256 _proposalID, uint256 _momentID) public constant returns (address);
     function momentValueOf(uint256 _proposalID, uint256 _momentID) public constant returns (uint256);
-    function momenTimeOf(uint256 _proposalID, uint256 _momentID) public constant returns (uint256);
+    function momentTimeOf(uint256 _proposalID, uint256 _momentID) public constant returns (uint256);
     function momentBlockOf(uint256 _proposalID, uint256 _momentID) public constant returns (uint256);
     function momentNonceOf(uint256 _proposalID, uint256 _momentID) public constant returns (uint256);
     function weightOf(uint256 _proposalID, uint256 _position) public constant returns (uint256);
@@ -17,8 +17,6 @@ contract IController {
     
     function numProposals() public constant returns (uint256 numProposals);
     function nonces(address _sender) public constant returns (uint256);
-    function changeRules(address _rules) public payable;
-    function forward(address _destination, uint _value, bytes _data) public;
     function newProposal(string _metadata, bytes32[] _data) public payable returns (uint proposalID);
     function vote(uint256 _proposalID, bytes32[] _data) public payable;
     function execute(uint256 _proposalID) public payable;
